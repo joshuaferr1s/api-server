8d0889a30e938be605e8d2f501dce51b4df117b2
Joshua Ferris <jajjferris@gmail.com>
2020-01-19 19:23:14 -0500

    :sparkles: initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# API Server
+
+> Server to host my apis
+
+## Hosted APIs
+
+> All apis hosted at https://api.jajjferris.com/[API_NAME]
---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.json({
+    status: 200,
+    hello: 'world!',
+  });
+});
+
+app.get('*', (req, res) => {
+  res.status(404).json({
+    status: 404,
+    msg: 'Endpoint not found!',
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    status: 500,
+    msg: err.message || 'Something broke!',
+  });
+});
+
+app.listen(port, () => console.log(`Listening at port ${port}`));
