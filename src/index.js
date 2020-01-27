841679c4fc711e3081330c7a73ec0c32a81c7433
Joshua Ferris <jajjferris@gmail.com>
2020-01-27 11:23:07 -0500

    added helmet

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 require('dotenv').config();
 
 const weather = require('./routes/weather');
@@ -11,6 +12,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(helmet());
 
 app.use('/weather', weather);
 app.use('/flag', flag);
